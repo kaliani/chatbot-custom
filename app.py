b55e9e25c3ec4ce25fb84b1c77049a213f324f0e
Ivan <kalyanivan@gmail.com>
2023-05-15 18:53:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ agent_selection = st.selectbox(
 
 if agent_selection == "Agent 1":
     uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-    run_csv_agent('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', uploaded_file)
+    run_csv_agent('', uploaded_file)
 elif agent_selection == "Agent 2":
     uploaded_file = st.file_uploader("**Upload Your PDF File**", type=["pdf"])
-    run_pdf_agent('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', uploaded_file)
+    run_pdf_agent('', uploaded_file)
 elif agent_selection == "Agent 3":
     text_input = st.text_area("Enter text for summarization", height=200)
     if st.button("Summarize"):
-        summary = run_summarize_agent('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', text_input)
+        summary = run_summarize_agent('', text_input)
         st.write("Summary:", summary)
